434e460787419c925f8af9e58a4c58740881bcaa
Adam Wilmore <adam@mx51.io>
2021-10-30 13:43:17 +1100

    Test with PyGithub

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Github Composite Action WIP
 
 
-Reference: https://docs.github.com/en/actions/creating-actions/creating-a-composite-action
+## References
+
+- https://docs.github.com/en/actions/creating-actions/creating-a-composite-action
+- https://medium.com/intelligentmachines/github-actions-building-and-publishing-own-actions-using-python-d94e2724b08c
--- action.py
+++ action.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import github
+import os
 import signal
 import sys
 
@@ -12,6 +14,48 @@ def main():
     print()
     print(' * Composite action test')
     print()
+
+    # Get github token
+    token = get_env_var('INPUT_TOKEN')
+    repo_name = get_env_var('GITHUB_REPOSITORY')
+
+    # Create client
+    gh = github.Github(token)
+    print(' * Github client created:')
+    print(gh)
+
+    # Display env vars
+    print(' * Displaying all env vars:')
+    show_all_env_vars()
+
+    # Finished
+    print()
+    print(' * Done.')
+    print()
+
+
+# Look up env var
+def get_env_var(env_var_name, strict=True):
+    # Check env var
+    value = os.getenv(env_var_name)
+
+    # Handle missing value
+    if not value:
+        if strict:
+            print(f'error: env var not found: {env_var_name}')
+            sys.exit(1)
+        else:
+            value = ''
+
+    return value
+
+
+# Display env vars
+def show_all_env_vars():
+    # Loop through env vars
+    for env_name in sorted(os.environ):
+        env_value = os.environ[env_name]
+        print('%-25s = %s' % (env_name, env_value))
 
 
 # Handle interrupt
