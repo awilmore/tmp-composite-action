748738e579be88cf751b5482278d201117bbf5c3
Adam Wilmore <adam@mx51.io>
2021-10-30 17:46:11 +1100

    Report on sonar results

--- action.py
+++ action.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from configparser import RawConfigParser
+from sonarqube import SonarQubeClient
+
 import github
 import json
 import os
@@ -11,7 +14,9 @@ import sys
 # GLOBALS
 ###
 
-COMMENT_TOKEN = 'SonarScan'
+SONAR_LOGO       = '![image](https://github.com/awilmore/tmp-composite-action/raw/master/images/sonar-logo-s.png'
+SONAR_PROPERTIES = 'sonar-project.properties'
+METRIC_KEYS      = 'coverage,code_smells,bugs'
 
 
 ###
@@ -27,7 +32,11 @@ def main():
         print(' * Not a pull request.')
         sys.exit()
 
-    print(f' * PR NUMBER: {pr_number}')
+    # Fetch sonar details
+    results = fetch_sonar_results()
+
+    # Generate PR comment
+    comment_body = generate_comment_body(results)
 
     # Get github params
     token = get_env_var('GITHUB_TOKEN')
@@ -39,12 +48,90 @@ def main():
     pr = repo.get_pull(pr_number)
 
     # Update PR with comment
-    pr.create_issue_comment(f'{COMMENT_TOKEN}: sonar scan complete.')
+    update_pr_comment(pr, comment_body)
 
     # Finished
     print()
     print(' * Done.')
     print()
+
+
+# Update PR with sonar scan comment
+def update_pr_comment(pr, comment_body):
+    # Retrieve comments to avoid duplicates
+    for c in pr.get_issue_comments():
+        if c.body == comment_body:
+            # Do not recreate duplicate comment
+            print(' * Sonar scan results comment already exists')
+            return
+
+    # Note: new comments will be added each time scan results change
+    print(' * Creating PR comment with sonar scan results')
+
+    # Create sonar scan comment
+    pr.create_issue_comment(comment_body)
+
+
+# Create PR comment body
+def generate_comment_body(results):
+    # Begin comment
+    comment = f'{SONAR_LOGO}  **Scan Results**:\n'
+
+    for key in sorted(results):
+        value = results[key]
+        if key == 'coverage':
+            value = f'{value}%'
+
+        comment += f' * **{key}**: {value}\n'
+
+    return comment.rstrip()
+
+
+# Fetch sonar results
+def fetch_sonar_results():
+    # Get sonar project key
+    sonar_project = read_sonar_project_key()
+
+    # Get sonar details
+    sonar_url = get_env_var('SONAR_HOST_URL')
+    sonar_token = get_env_var('SONAR_TOKEN')
+
+    # Create sonar client
+    sonar = SonarQubeClient(sonarqube_url=sonar_url, token=sonar_token)
+
+    # Find project details
+    component = sonar.measures.get_component_with_specified_measures(component=sonar_project, version="dev-another-pr-1", fields="metrics,periods", metricKeys=METRIC_KEYS)
+    measures = component['component']['measures']
+
+    # Parse results
+    results = {}
+    for metric in measures:
+        field = metric['metric']
+        value = metric['value']
+        results[field] = value
+
+    # Check for coverage
+    if 'coverage' not in results:
+        results['coverage'] = 0
+
+    # Return results
+    return results
+
+
+# Read sonar-project.properties file
+def read_sonar_project_key():
+    # Read sonar properties
+    with(open(SONAR_PROPERTIES, 'r')) as f:
+        for prop in [line.rstrip() for line in f]:
+            name, value = prop.split('=')
+
+            # Check property
+            if name == 'sonar.projectKey':
+                return value
+
+    # Something went wrong
+    print(f'error: sonar.projectKey value not found in sonar properties file: {SONAR_PROPERTIES}')
+    sys.exit(1)
 
 
 # Get PR number from event details
