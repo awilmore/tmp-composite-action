a2c737ca9b5d15d3f6028ff5c4dc4255f6e12dcd
Adam Wilmore <adam@mx51.io>
2021-10-30 14:58:02 +1100

    More testing

--- action.py
+++ action.py
@@ -15,23 +15,50 @@ def main():
     print(' * Composite action test')
     print()
 
-    # Get github token
+    # Get github params
     token = get_env_var('GITHUB_TOKEN')
     repo_name = get_env_var('GITHUB_REPOSITORY')
+    #pr_number = get_env_var('', strict=False)
 
     # Create client
     gh = github.Github(token)
     print(' * Github client created:')
     print(gh)
 
-    # Display env vars
-    print(' * Displaying all env vars:')
-    show_all_env_vars()
+    ## Display env vars
+    #print(' * Displaying all env vars:')
+    #show_all_env_vars()
+
+    # Get event details
+    event_json = read_event()
+    print(' * event_json:')
+    print(event_json)
+    print()
+
+    # List PR comments
+#    if pr_number:
+#        repo = gh.get_repo(repo_name)
+#        pr = repo.get_pull(pr_number)
+#
+#    else:
+#        print(' * No Pull Request number detected.')
 
     # Finished
     print()
     print(' * Done.')
     print()
+
+
+# Read github event data
+def read_event():
+    # Find path
+    event_path = gen_env_var('GITHUB_EVENT_PATH')
+
+    # Read json contents
+    with open(event_path, 'r') as f:
+        json_data = json.load(f)
+
+    return json_data
 
 
 # Look up env var
@@ -56,6 +83,7 @@ def get_env_var(env_var_name, strict=True):
     return value
 
 
+# TODO - remove:
 # Display env vars
 def show_all_env_vars():
     # Loop through env vars
